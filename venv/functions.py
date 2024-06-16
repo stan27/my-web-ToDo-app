2fc993cc96ea3ae178b877ec0a8b163d588779e8
stasd <stasdiduk@gmail.com>
2024-06-16 20:41:55 +0300

    Making some changes to web.py and functions.py

--- venv/functions.py
+++ venv/functions.py
@@ -16,6 +16,6 @@ def write_todos(todos_arg, filepath=FILEPATH):
         file.writelines(todos_arg)
 
 
-if __name__ == "__main__":
-    print("Hello")
-    print(get_todos())
+# if __name__ == "__main__":
+#    print("Hello")
+#    print(get_todos())
